1045bf8b08db7696d8c7cdffac3a0cc3b8529963
Anant <anantvijay.23@gmail.com>
2017-05-08 23:30:46 +0100

    Team Player Cards

--- javascript/sfc.js
+++ javascript/sfc.js
@@ -26,50 +26,53 @@ function handleJoinTeamButtonClick() {
 	var fbImg = document.createElement("img");
 	var gFormImg = document.createElement("img");
 
-	outerDiv.setAttribute("onfoucsout","handleCancelButtonClick(joinTeamPopup)");
-	outerDiv.setAttribute("id","joinTeamPopup");
-	outerDiv.setAttribute("class","popupMessage mdl-shadow--2dp");
-	call.setAttribute("href","tel: +919871790011");
-	facebook.setAttribute("href","https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Fmessages%2Ft%2Fuser%3A442830629124075");
-	facebook.setAttribute("target","_blank");
-	googleForm.setAttribute("href","https://goo.gl/forms/tMiItj1J8U0ckzBK2");
-	googleForm.setAttribute("target","_blank");
-	fbImg.setAttribute("src","images/logos/Messenger_Icon.png")
-	fbImg.setAttribute("class","logos");
-	gFormImg.setAttribute("src","images/logos/logo_forms_48px.png")
-	gFormImg.setAttribute("class","logos");
-	callImg.setAttribute("class","logos");
-	callImg.setAttribute("src","images/logos/ic_call_black_48dp.png");
-	cancelButton.setAttribute("class","mdl-button mdl-js-button");
-	cancelButton.setAttribute("id","cancelButton");
-	cancelButton.setAttribute("onclick","handleCancelButtonClick(this.parentElement.id)");
-	cancelButtonIcon.setAttribute("class","material-icons");
+	function createPopUp () {
+
+		outerDiv.setAttribute("id","joinTeamPopup");
+		outerDiv.setAttribute("class","popupMessage mdl-shadow--2dp");
+		call.setAttribute("href","tel: +919871790011");
+		facebook.setAttribute("href","https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Fmessages%2Ft%2Fuser%3A442830629124075");
+		facebook.setAttribute("target","_blank");
+		googleForm.setAttribute("href","https://goo.gl/forms/tMiItj1J8U0ckzBK2");
+		googleForm.setAttribute("target","_blank");
+		fbImg.setAttribute("src","images/logos/Messenger_Icon.png")
+		fbImg.setAttribute("class","logos");
+		gFormImg.setAttribute("src","images/logos/logo_forms_48px.png")
+		gFormImg.setAttribute("class","logos");
+		callImg.setAttribute("class","logos");
+		callImg.setAttribute("src","images/logos/ic_call_black_48dp.png");
+		cancelButton.setAttribute("class","mdl-button mdl-js-button");
+		cancelButton.setAttribute("id","cancelButton");
+		cancelButton.setAttribute("onclick","handleCancelButtonClick(this.parentElement.id)");
+		cancelButtonIcon.setAttribute("class","material-icons");
 
 
-	cancelButtonIcon.innerHTML = "cancel";
+		cancelButtonIcon.innerHTML = "cancel";
 
+		call.appendChild(callImg);
+		facebook.appendChild(fbImg);
+		googleForm.appendChild(gFormImg);
+		outerDiv.appendChild(call);
+		outerDiv.appendChild(facebook);
+		outerDiv.appendChild(googleForm);
+		cancelButton.appendChild(cancelButtonIcon);
+		outerDiv.appendChild(cancelButton);
+		contentBox.appendChild(outerDiv);
+	};
 
-	call.appendChild(callImg);
-	facebook.appendChild(fbImg);
-	googleForm.appendChild(gFormImg);
-	outerDiv.appendChild(call);
-	outerDiv.appendChild(facebook);
-	outerDiv.appendChild(googleForm);
-	cancelButton.appendChild(cancelButtonIcon);
-	outerDiv.appendChild(cancelButton);
-	contentBox.appendChild(outerDiv);
+	createPopUp();
 
 	// ----------------- Mamu Review -------------------------------
 	// Code to bring the div in focus
 	// event to listen to div going out of focus
 	// trigger event handleCancelButtonClick which takes div id and deletes it
 
-	document.getElementById("joinTeamPopup").focus();
-
-	var focusOutBox = document.getElementById("joinTeamPopup");
-	focusOutBox.addEventListener("focusout", handleCancelButtonClick(outerDiv.id));
-
 }
+
+function focusMethod(elementId) {
+	console.log(elementId);
+	document.getElementById(elementId).focus();
+};
 
 // Test function to work within same-orgin security policies
 // add focus property
