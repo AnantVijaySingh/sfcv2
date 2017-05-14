ecaf1e2f79afb2e55efbcbfd4e4143225edb03e7
Anant <anantvijay.23@gmail.com>
2017-05-14 20:13:13 +0100

    Reading Player Data from JSON

--- javascript/team.js
+++ javascript/team.js
@@ -1,130 +1,50 @@
-var terms
-var contentBox
-var playMatchButton
-var joinTeamButton
+var teamMainContent;
 
-window.onload = function() {
-	terms = document.getElementById('terms');
-	contentBox  = document.getElementById('contentBox');
-	playMatchButton = document.getElementById('playMatch');
-	joinTeamButton = document.getElementById('joinTeam');
-
-	terms.onclick = handleTermsClick;
-	joinTeamButton.onclick = handleJoinTeamButtonClick;
+window.onload = function () {
+	contentDiv = document.getElementById('teamMainContent');
+	parsePlayerJSON();
+	createPlayerCard();
 }
 
-function handleJoinTeamButtonClick() {
-
-	var outerDiv = document.createElement("div");
-	var call = document.createElement("a");
-	var facebook = document.createElement("a");
-	var googleForm = document.createElement("a");
-	var cancelButton = document.createElement("button");
-	var cancelButtonIcon = document.createElement("i");
-	var callImg = document.createElement("img");
-	var fbImg = document.createElement("img");
-	var gFormImg = document.createElement("img");
-
-	function createPopUp () {
-
-		outerDiv.setAttribute("id","joinTeamPopup");
-		outerDiv.setAttribute("class","popupMessage mdl-shadow--2dp");
-		call.setAttribute("href","tel: +919871790011");
-		facebook.setAttribute("href","https://www.facebook.com/login.php?next=https%3A%2F%2Fwww.facebook.com%2Fmessages%2Ft%2Fuser%3A442830629124075");
-		facebook.setAttribute("target","_blank");
-		googleForm.setAttribute("href","https://goo.gl/forms/tMiItj1J8U0ckzBK2");
-		googleForm.setAttribute("target","_blank");
-		fbImg.setAttribute("src","images/logos/Messenger_Icon.png")
-		fbImg.setAttribute("class","logos");
-		gFormImg.setAttribute("src","images/logos/logo_forms_48px.png")
-		gFormImg.setAttribute("class","logos");
-		callImg.setAttribute("class","logos");
-		callImg.setAttribute("src","images/logos/ic_call_black_48dp.png");
-		cancelButton.setAttribute("class","mdl-button mdl-js-button");
-		cancelButton.setAttribute("id","cancelButton");
-		cancelButton.setAttribute("onclick","handleCancelButtonClick(this.parentElement.id)");
-		cancelButtonIcon.setAttribute("class","material-icons");
-
-
-		cancelButtonIcon.innerHTML = "cancel";
-
-		call.appendChild(callImg);
-		facebook.appendChild(fbImg);
-		googleForm.appendChild(gFormImg);
-		outerDiv.appendChild(call);
-		outerDiv.appendChild(facebook);
-		outerDiv.appendChild(googleForm);
-		cancelButton.appendChild(cancelButtonIcon);
-		outerDiv.appendChild(cancelButton);
-		contentBox.appendChild(outerDiv);
+//-------------------Update PlayerJSON url when publishing -------------------
+function parsePlayerJSON() {
+	var url = "https://github.com/AnantVijaySingh/sfcv2/blob/master/content/players.json";
+	var request = new XMLHttpRequest();
+	request.open("GET", URL);
+	request.onload = function() {
+		if (request.status == 200) {
+			console.log(request.responseText);
+		}
 	};
-
-	createPopUp();
-
-	// ----------------- Mamu Review -------------------------------
-	// Code to bring the div in focus
-	// event to listen to div going out of focus
-	// trigger event handleCancelButtonClick which takes div id and deletes it
-
+	request.sedn(null);
 }
 
-// Test function to work within same-orgin security policies
-// add focus property
-function handleTermsClick() {
 
-	var outerDiv = document.createElement("div");
-	var innerPara = document.createElement("p");
-	var cancelButton = document.createElement("button");
-	var cancelButtonIcon = document.createElement("i");
+function createPlayerCard() {
+	var outerDiv = document.createElement('div');
+	var photoDiv = document.createElement('div');
+	var discriptionDiv = document.createElement('div');
+	var playerNameH2 = document.createElement('h2');
+	var buttonDiv = document.createElement('div');
+	var popupButton = document.createElement('a');
 
-	outerDiv.setAttribute("id","popupMessageId");
-	outerDiv.setAttribute("class","popupMessage mdl-shadow--2dp");
-	cancelButton.setAttribute("class","mdl-button mdl-js-button");
-	cancelButton.setAttribute("id","cancelButton");
-	cancelButton.setAttribute("onclick","handleCancelButtonClick(this.parentElement.id)");
-	cancelButtonIcon.setAttribute("class","material-icons");
+	outerDiv.setAttribute('class','player-card mdl-card');
+	photoDiv.setAttribute('class','mdl-card__title mdl-card--expand');
+	playerNameH2.setAttribute('class','mdl-card__title-text');
+	discriptionDiv.setAttribute('class','mdl-card__supporting-text');
+	popupButton.setAttribute('class','mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect');
+	buttonDiv.setAttribute('class','mdl-card__actions mdl-card--border');
 
-	cancelButtonIcon.innerHTML = "cancel";
-	innerPara.innerHTML = "Testing UI";
+	popupButton.innerHTML = 'Read Bio';
+	playerNameH2.innerHTML = 'Anant Vijay';
+	discriptionDiv.innerHTML = 'Midfielder with 5 Goals and 27 Assists in 150 appearcnes for the club.';
 
-	outerDiv.appendChild(innerPara);
-	cancelButton.appendChild(cancelButtonIcon);
-	outerDiv.appendChild(cancelButton);
-	contentBox.appendChild(outerDiv);
+	buttonDiv.appendChild(popupButton);
+	photoDiv.appendChild(playerNameH2);
+	outerDiv.appendChild(photoDiv);
+	outerDiv.appendChild(discriptionDiv);
+	outerDiv.appendChild(buttonDiv);
+	contentDiv.appendChild(outerDiv);
+
+
 }
-
-function handleCancelButtonClick(divElement) {
-	console.log(divElement);
-	var deleteDiv = document.getElementById(divElement);
-	deleteDiv.parentNode.removeChild(deleteDiv);
-}
-
-// -------------------------------Actual function---------------------------------
-// function handleTermsClick() {
-
-// 	var outerDiv = document.createElement("div");
-// 	var innerPara = document.createElement("p");
-
-// 	outerDiv.setAttribute("id","popupMessageId");
-// 	outerDiv.setAttribute("class","popupMessage");
-
-// 	var termsAndConditionsFile = "content/TandC.txt"
-
-//     var textFile = new XMLHttpRequest();
-//     textFile.open("GET", termsAndConditionsFile, false);
-//     textFile.onreadystatechange = function ()
-//     {
-//         if(textFile.readyState === 4)
-//         {
-//             if(textFile.status === 200 || textFile.status == 0)
-//             {
-//                 var allText = textFile.responseText;
-//                 innerPara.innerText = allText 
-//             }
-//         }
-//     }
-//     textFile.send(null);
-
-// 	outerDiv.appendChild(innerPara);
-// 	contentBox.appendChild(outerDiv);
-// }
