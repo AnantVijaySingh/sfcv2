4eefe459134a03add38ac606c6820bffbfb9bd42
Anant <anantvijay.23@gmail.com>
2017-05-14 21:51:41 +0100

    building player cards through JSON

--- javascript/team.js
+++ javascript/team.js
@@ -8,30 +8,25 @@ window.onload = function () {
 
 //-------------------Update PlayerJSON url when publishing -------------------
 function parsePlayerJSON() {
+	var players;
 	var url1 = "https://anantvijaysingh.github.io/sfcv2/content/players.json";
 	var request = new XMLHttpRequest();
 	request.open("GET", url1);
 	request.onload = function () {
 		if (request.status == 200) {
 			console.log(request.responseText);
+			players = JSON.parse(responseText);
+
+			for (var i = 0; i < players.length; i++) {
+				createPlayerCard(players[i]);
+			}
 		}
 	};
 	request.send(null);
-
-	var url = "../content/players.json";
-	var playerData = new XMLHttpRequest();
-    playerData.open("GET", url);
-    playerData.onload = function () {
-        if(playerData.status == 200)
-        {
-        	console.log(playerData.responseText);
-        }
-    }
-    playerData.send(null);
 }
 
 
-function createPlayerCard() {
+function createPlayerCard(playerObject) {
 	var outerDiv = document.createElement('div');
 	var photoDiv = document.createElement('div');
 	var discriptionDiv = document.createElement('div');
@@ -41,14 +36,17 @@ function createPlayerCard() {
 
 	outerDiv.setAttribute('class','player-card mdl-card');
 	photoDiv.setAttribute('class','mdl-card__title mdl-card--expand');
+	photoDiv.setAttribute('style','background-image: url(../images/club/anantvijay.jpg)');
 	playerNameH2.setAttribute('class','mdl-card__title-text');
 	discriptionDiv.setAttribute('class','mdl-card__supporting-text');
 	popupButton.setAttribute('class','mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect');
 	buttonDiv.setAttribute('class','mdl-card__actions mdl-card--border');
 
 	popupButton.innerHTML = 'Read Bio';
-	playerNameH2.innerHTML = 'Anant Vijay';
-	discriptionDiv.innerHTML = 'Midfielder with 5 Goals and 27 Assists in 150 appearcnes for the club.';
+	playerNameH2.innerHTML = playerObject.firstName;
+	discriptionDiv.innerHTML = playerObject.position + ' with ' + playerObject.stats.goals + 'Goals and ' + 
+								playerObject.stats.assists + ' Assists in ' + playerObject.stats.appearences 
+								+ ' appearcnes for the club.';
 
 	buttonDiv.appendChild(popupButton);
 	photoDiv.appendChild(playerNameH2);
@@ -56,6 +54,4 @@ function createPlayerCard() {
 	outerDiv.appendChild(discriptionDiv);
 	outerDiv.appendChild(buttonDiv);
 	contentDiv.appendChild(outerDiv);
-
-
 }
